3a55cd168bfad2041549efd8b571d8e232740a46
Daniela <daniela-bello30>
2025-12-17 19:03:46 -0500

    modulo de citas

---
+++ Views/BookAppointmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookAppointmentViewController.swift
+//  ProyectoDAMII
+//
+//  Created by daniela on 17/12/25.
+//
+
+import UIKit
+
+class BookAppointmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
